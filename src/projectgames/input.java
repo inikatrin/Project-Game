eeb65817816aa0af8244c10f52ad84da1ce3d84c
Personal <Personal@DESKTOP-2S7P7RR>
2018-11-14 10:34:15 +0700

    vers 1.1

---
+++ src/projectgames/input.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectgames;
+
+/**
+ *
+ * @author Personal
+ */
+public class input {
+    private boolean isMenu=true;
+    
+    public void keyBoard(){
+        
+    }
+    
+    public void mouse(){
+        
+    }
+}
